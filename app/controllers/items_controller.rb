0201bb36c705dd8796a68533cb180f9cc189f352
carreuky <carreuky@gmail.com>
2022-09-27 21:54:53 +0300

    Done.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+    if params[:user_id]
+      user=User.find(params[:user_id])
+      items=user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    user=Item.find(params[:id])
+    render json: user, include: :user
+  end
+
+  def create
+    review = Item.create(user_params)
+    render json: review, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :city)
+  end
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+  
 end
